0e1b5de5d25ee5ae3d2316981086eeeac87043cd
josmengom <josmengom@8d732c57-1a7d-2282-3cdd-05197929a1e1>
2011-12-28 22:11:05 +0000

    Modifico las  bd, como dice Juan Carlos los tablas usuario-apli, usuario-juego son INUTILES.

--- trunk/ABD.sql
+++ trunk/ABD.sql
@@ -3,11 +3,6 @@
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
 -- Base de datos: `abd`
 
 CREATE DATABASE IF NOT EXISTS `abd` DEFAULT CHARACTER SET latin1 COLLATE latin1_general_ci;
@@ -44,40 +39,6 @@ CREATE TABLE IF NOT EXISTS `Juegos` (
 --Introduccion de tuplas en la tabla `Juegos`
 
 
--- Tabla `usuario-aplicacion`
-DROP TABLE IF EXISTS `usuario-aplicacion`;
-CREATE TABLE IF NOT EXISTS `usuario-aplicacion` (
-  `idUsuario` int(10) unsigned NOT NULL,
-  `idAplicacion` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`idusuario`,`idAplicacion`),
-  KEY `FK_usuarioevento_2` (`idAplicacion`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- Introduccion de tuplas en la tabla `usuario-aplicacion`
-INSERT INTO `usuarioevento` (`idUsuario`, `idAplicacion`) VALUES
-(3, 3),
-(4, 3),
-(4, 4),
-(4, 5);
-
-
-
--- Tabla `usuario-juego`
-DROP TABLE IF EXISTS `usuario-juego`;
-CREATE TABLE IF NOT EXISTS `usuario-juego` (
-  `idUsuario` int(10) unsigned NOT NULL,
-  `idJuego` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`idusuario`,`idJuego`),
-  KEY `FK_usuarioevento_2` (`idJuego`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- Introduccion de tuplas en la tabla `usuario-juego`
-INSERT INTO `usuario-juego` (`idUsuario`, `idJuego`) VALUES
-(3, 3),
-(4, 3),
-(4, 4),
-(4, 5);
-
 
 
 
@@ -108,18 +69,14 @@ INSERT INTO `usuarios` (`idusuario`, `usuario`, `password`, `nombre`, `apellidos
 
 -- Constraint para tabla `usuario-aplicacion`
 
-ALTER TABLE `usuario-aplicacion`
-  ADD CONSTRAINT `FK_aplicacion_1` FOREIGN KEY (`idAplicacion`) REFERENCES `Aplicaciones` (`idAplicacion`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `Aplicacion`
   ADD CONSTRAINT `FK_aplicacion_2` FOREIGN KEY (`idUsuario`) REFERENCES `Usuarios` (`idUsuario`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraint para tabla `usuario-juego`
 --
-ALTER TABLE `usuario-juego`
-  ADD CONSTRAINT `FK_Juego_1` FOREIGN KEY (`idJuego`) REFERENCES `Juegos` (`idJuegos`) ON DELETE CASCADE ON UPDATE CASCADE,
+ALTER TABLE `Juego`
   ADD CONSTRAINT `FK_Juego_2` FOREIGN KEY (`idUsuario`) REFERENCES `Usuarios` (`idUsuario`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+

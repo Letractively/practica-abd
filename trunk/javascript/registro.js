28daabea0b70768530eee7f15463f26948325ed9
JuanC.Prieto.Silos@gmail.com <JuanC.Prieto.Silos@gmail.com@8d732c57-1a7d-2282-3cdd-05197929a1e1>
2011-12-29 22:12:52 +0000

    Eliminacion de las tablas en registro.html y sustitucion por bloques <div>. Añadida la verificacion del formulario a nivel cliente con javascript. Se comprueban nick, contraseña e e-mail. Tambien se comprueba que la imagen para el avatar sea o en jpg o en png.

---
+++ trunk/javascript/registro.js
@@ -0,0 +1,43 @@
+/**
+ * @author Juanky
+ */
+function validarFormulario() {
+	var nick= document.getElementById("nick").value;
+	var password= document.getElementById("password").value;
+	var mail= document.getElementById("mail").value;
+	var patronEmail=/^(.+)@(.+)$/;
+	if (nick==""){
+		alert("Debe introducir un nick");
+		return false;
+	}
+	if (password=="" || password.length<4 || password.length>8 ){
+		alert("La contraseña debe tener entre 4 y 8 caracteres");
+		return false;
+	}
+	if (mail=="" || patronEmail.test(mail)==false){
+		alert("Debe de introducir una cuenta de mail valida");
+		return false;
+	}
+	if (verificarContraseña()==false){
+		return false;
+	}
+}
+
+function verificarContraseña(){
+	var password= document.getElementById("password").value;
+	var passwordBis= document.getElementById("passwordBis").value;
+	if (password!=passwordBis){
+		alert ("Las contraseñas deben de ser identicas");
+		return false;
+	}
+}
+
+function validarImagen(){
+	var ext = document.getElementById("foto").value;
+	ext = ext.substring(ext.length-3,ext.length);
+	ext = ext.toLowerCase();
+	if(ext != "jpg" && ext!="jpeg" && ext!="png") {
+		alert("Solo son valida imagenes en formato jpeg o png");
+		document.getElementById("foto").value="";
+	}
+}

c5d98a1ad03fb11cbbdc8068b6551287380c5a03
JuanC.Prieto.Silos <JuanC.Prieto.Silos@gmail.com>
2012-01-08 21:00:26 +0000

    Cambiamos los archivos de html a php. Restauramos la validacion del formulario de registro en javascript a su version buena. Implementacion de la validacion del formulario en PHP y la insercion en la BD. Creacion del archivo SQL para la creacion de la tabla Usuario en la BD OpenDown ya que el archivo ABD.sql tiene tropomil errores y no hay manera de hacerlo andar.

---
+++ php/tratarRegistro.php
@@ -0,0 +1,107 @@
+<?php
+	include_once("gestionBD.php");
+
+	session_start();
+	$registro= $_SESSION["registro"];
+	if (isset($registro)){
+		$registro["nick"]=$_REQUEST["nick"];
+		$registro["password"]=$_REQUEST["password"];
+		$registro["passwordBis"]=$_REQUEST["passwordBis"];
+		$registro["mail"]=$_REQUEST["mail"];
+		$registro["nombre"]=$_REQUEST["nombre"];
+		$registro["apellidos"]=$_REQUEST["apellidos"];
+		$registro["poblacion"]=$_REQUEST["poblacion"];
+		$registro["provincia"]=$_REQUEST["provincia"];
+		$registro["codigoPostal"]=$_REQUEST["codigoPostal"];
+		$registro["sexo"]=$_REQUEST["sexo"];
+		$registro["foto"]=$_FILES["foto"];
+		$_SESSION["registro"]=$registro;
+		if(verificaDatos($registro)){
+			if (!insertarUsuario($registro)){
+				array_push($_SESSION["erroresRegistro"],"Se ha producido un error, intenrelo de nuevo");
+				header("Location: ../registro.php");
+			}else{
+				header("Location: ../index.php");	
+			}
+		}else{
+			header("Location: ../registro.php");
+		}
+	}else{
+		header("Location: ../registro.php");
+	}
+	
+	function verificaDatos($registro){
+		$verificado=true;
+		$errores=array();
+		$patron='/^(.+)@(.+)\.(.+)$/';
+		if($registro["nick"]==""){
+			array_push($errores,"Introduzca el nick de usuario");
+			$verificado=false;
+		}elseif(nombreOcupado($registro["nick"])){
+			array_push($errores,"Ese nick ya esta en uso");
+			$verificado=false;
+		}
+		if (strlen($registro["password"])<4 || strlen($registro["password"])>8){
+			array_push($errores,"La contraseña debe tener entre 4 y 8 caracteres");
+			$verificado=false;
+		}else if($registro["password"]!=$registro["passwordBis"]){
+			array_push($errores,"Las contraseñas deben de ser identicas");
+			$verificado=false;
+		}
+		if ($registro["mail"]==""){
+			array_push($errores,"Debe introducir una direccion de correo");
+			$verificado=false;
+		}elseif(!preg_match($patron,$registro["mail"])){
+			array_push($errores,"Debe introducir una direccion de correo valida");
+			$verificado=false;
+		}
+		/**if($_FILES["foto"]["type"]!="image/jpeg" || $_FILES["foto"]["type"]!="image/png"){
+			array_push($errores,"Solo son valida imagenes en formato jpeg o png");
+			$verificado=false;
+		}*/
+		$_SESSION["erroresRegistro"]=$errores;
+		return $verificado;
+	}
+	
+	function nombreOcupado($nick){
+		$conexion=crearConexionBD();
+		$ocupado=false;
+		try{
+			$stmt=$conexion->prepare("SELECT * FROM Usuarios WHERE nick=:nick");
+			$stmt->bindParam(':nick',$nick);
+			$stmt->execute();
+			$usuario=$stmt->fetch();
+			if($usuario!=null){
+				$ocupado=true;
+			}
+		}catch(PDOException $e){
+			$_SESSION["exception"]="Error al obtener los datos del usuario";
+			//header("Location: ../exception.php");
+			echo "Imposible leer de la BD";
+			die();
+		}
+		cerrarConexionBD($conexion);
+		return $ocupado;
+	}
+	
+	function insertarUsuario($registro){
+		$conexion=crearConexionBD();
+		$insertado=false;
+		$fecha= date("Y-m-d",time());
+		try{
+			$query="insert into Usuarios (nick, pass, mail, fechaRegistro, nombre, apellidos, poblacion, 
+			provincia, codigoPostal, sexo, foto) values ('$registro[nick]', '$registro[password]', '$registro[mail]', 
+			'$fecha', '$registro[nombre]', '$registro[apellidos]', '$registro[poblacion]', '$registro[provincia]', 
+			'$registro[codigoPostal]', '$registro[sexo]', '$registro[foto]');";
+			$conexion->exec($query);
+			$insertado=true;
+		}catch(PDOException $e){
+			$_SESSION["exception"]="Error al obtener los datos del usuario";
+			//header("Location: ../exception.php");
+			echo "Imposible escribir en la BD";
+			die();
+		}
+		cerrarConexionBD($conexion);
+		return $insertado;
+	}
+?>

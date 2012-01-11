ff148c5edb080a09f643bc3e2e98aaa8c9c3015f
josmengom <josmengom@8d732c57-1a7d-2282-3cdd-05197929a1e1>
2012-01-11 19:51:52 +0000

    Borro el opendown.sql, y reparo el ABD.sql, ya funciona el script, por lo visto este compilador no se traga los comentarios --

--- ABD.sql
+++ ABD.sql
@@ -1,15 +1,10 @@
 
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
-
--- Base de datos: `abd`
-
-CREATE DATABASE IF NOT EXISTS `abd` DEFAULT CHARACTER SET latin1 COLLATE latin1_general_ci;
+CREATE DATABASE IF NOT EXISTS `abd`;
 USE abd;
 
 
--- Tabla `Aplicaciones`
+
 DROP TABLE IF EXISTS `Aplicaciones`;
 CREATE TABLE IF NOT EXISTS `Aplicaciones` (
   `idAplicacion` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -17,14 +12,14 @@ CREATE TABLE IF NOT EXISTS `Aplicaciones` (
   `Descripcion` text CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
   `Foto` varchar(30) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
   `idUsuario` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`idAplicaciones`),
+  PRIMARY KEY (`idAplicacion`),
   KEY `FK_evento_1` (`idusuario`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
---Introduccion de tuplas en la tabla `Aplicaciones`
 
 
--- Tabla `Juegos`
+
+
 DROP TABLE IF EXISTS `Juegos`;
 CREATE TABLE IF NOT EXISTS `Juegos` (
   `idJuego` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -36,47 +31,33 @@ CREATE TABLE IF NOT EXISTS `Juegos` (
   KEY `FK_evento_1` (`idusuario`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
---Introduccion de tuplas en la tabla `Juegos`
 
 
 
 
 
---  Tabla `usuarios`
-DROP TABLE IF EXISTS `usuarios`;
-CREATE TABLE IF NOT EXISTS `usuarios` (
-  `idusuario` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `usuario` varchar(45) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
-  `password` varchar(45) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
-  `nombre` varchar(45) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
-  `apellidos` varchar(45) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
-  `email` varchar(45) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
-  `poblacion` varchar(45) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
-  `provincia` varchar(45) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
-  `cp` int(5) unsigned NOT NULL,
-  `sexo` char(10) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
-  `avatar` varchar(30) CHARACTER SET latin1 COLLATE latin1_general_ci,
-  PRIMARY KEY (`idusuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=1 ;
-
--- Introduccion de tuplas en la tabla `usuario`
-INSERT INTO `usuarios` (`idusuario`, `usuario`, `password`, `nombre`, `apellidos`, `email`, `poblacion`, `provincia`, `cp`, `sexo`, `foto`) VALUES
-(1, 'Jose', 'Jose', 'Jose', 'Mena Gomez', 'josmengom@gmail.com', 'Cadiz', 'Cadiz', 11600, 'hombre', 'mena.JPG'),
-(2, 'Javier', 'Javier', 'Javier', 'B', 'javierb@gmail.com', 'Sevilla', 'Sevilla', 41005, 'hombre', 'javi.JPG')
-(3, 'Juanky', 'qwerty', 'Juan Carlos', 'Prieto', 'JuanC.Prieto.Silos@gmail.com', 'camas', 'Sevilla', 41900, 'hombre', null)
+drop table if exists Usuarios;
+create table if not exists Usuarios(
+	idUsuario int(10) not null AUTO_INCREMENT,
+	nick varchar(16) not null,
+	pass varchar(8) not null,
+	mail varchar(30) not null,
+	fechaRegistro date not null,
+	nombre varchar(30),
+	apellidos varchar(30),
+	poblacion varchar(30),
+	provincia varchar(30),
+	codigoPostal int(5),
+	sexo varchar(6),
+	foto varchar(100),
+	PRIMARY KEY (idUsuario))
+	ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+	
+insert into Usuarios (nick, pass, mail, fechaRegistro, nombre, apellidos, poblacion, provincia, codigoPostal, sexo) values
+	('Juanky', '1234', 'JuanC.Prieto.Silos@gmail.com', '2012-01-01', 'Juan Carlos', 'Prieto', 'Camas', 'Sevilla', '41900', 'hombre');
 
 
 
--- Constraint para tabla `Aplicacion`
-
-ALTER TABLE `Aplicacion`
-  ADD CONSTRAINT `FK_aplicacion_2` FOREIGN KEY (`idUsuario`) REFERENCES `Usuarios` (`idUsuario`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraint para tabla `Juego`
---
-ALTER TABLE `Juego`
-  ADD CONSTRAINT `FK_Juego_2` FOREIGN KEY (`idUsuario`) REFERENCES `Usuarios` (`idUsuario`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 
 

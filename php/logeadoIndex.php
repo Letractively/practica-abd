de38760f226cdcb885e93a270b0ad0647da073bf
josmengom <josmengom@8d732c57-1a7d-2282-3cdd-05197929a1e1>
2012-01-12 22:32:03 +0000

    este es provisional, supuestamente este debe de comprobar que elusuario esta registrado, y darle valores a las variables de sesion , para poder ver el div de menu"oculto " en el index.

---
+++ php/logeadoIndex.php
@@ -0,0 +1,47 @@
+<?php
+		//Iniciamos la sesion e importamos las librerias necesarias
+		
+		session_start();
+		include_once("gestionarConexionBD.php");
+		include_once("gestionUsuarios.php");
+		include_once("funciones.php");
+		
+		$login=$_SESSION["login"];//Recuperamos la variable creada en index.php
+		$errores=array();
+		if(isset($login)){/*si no existe la variable login quiere decir que no viene de la pagina index.php*/
+			$login["usuario"]=$_REQUEST["usuario"];//Guardamos los valores enviados en el formulario
+			$login["password"]=$_REQUEST["password"];
+			if(esNulo($login,"usuario"))
+				array_push($errores,"Introduzca su nombre de usuario");
+			if(esNulo($login,"password"))
+				array_push($errores,"Introduzca su contrase&ntilde;a");
+			$_SESSION["errores_index"]=$errores;
+			if(count($errores)==0){//Si no hay errores nos conectamos a la BD y comprobamos que el usuario esta registrado
+				$conexion=CrearConexionBD();
+				$registrado=estaRegistrado($login["usuario"],$login["password"],$conexion);
+				if($registrado){
+					unset($_SESSION["errores_index"]);//Borramos los  errores de index.php
+					$usuario=getUsuario($login["usuario"],$conexion);
+					CerrarConexionBD($conexion);
+					$estasDentro=array();//Creamos una variable para saber que nos hemos logueado
+					$estasDentro["idusuario"]=$usuario["idusuario"];//Guardamos los datos que utilizaremos una vez dentro
+					$estasDentro["usuario"]=$usuario["usuario"];
+					$estasDentro["foto"]=$usuario["foto"];
+					$_SESSION["estasDentro"]=$estasDentro;//Y la guardamos en la sesion
+					header("Location: ../index.php");//Redireccionamos a index
+				}else{//Si se produce algun error redireccionamos a index.php y guardamos el error, pero en este caso no lleva el estasDentro
+					CerrarConexionBD($conexion);
+					array_push($errores,"Debes estar registrado");
+					$_SESSION["errores_index"]=$errores;
+					header("Location: ../index.php");
+					
+				}
+			}else{
+				header("Location: ../index.php");
+				}
+				
+			
+		}else{
+			header("Location: ../index.php");
+			}
+?>
